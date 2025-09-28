cc121d29bdc671b29ffa06d55f53338c5304a812
Humble Guy <mega.sirozu@gmail.com>
2025-09-28 10:25:19 +0300

    Time: 35 ms (23.54%), Space: 44.3 MB (49.31%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s)
+    {
+        return GetMaxSubStr(s);
+    }
+
+    private int GetMaxSubStr(string s)
+    {
+        var charsIndexes = new Dictionary<char, int>();
+        int maxCounter = 0;
+        for (int i = 0; i < s.Length; ++i)
+        {
+            var ch = s[i];
+            if (charsIndexes.TryGetValue(ch, out var index))
+            {
+                if (charsIndexes.Count > maxCounter)
+                {
+                    maxCounter = charsIndexes.Count;
+                }
+                charsIndexes.Clear();
+                i = index;
+            }
+            else
+            {
+                charsIndexes[ch] = i;
+            }
+        }
+
+        return charsIndexes.Count > maxCounter ? charsIndexes.Count : maxCounter;
+    }
+}
